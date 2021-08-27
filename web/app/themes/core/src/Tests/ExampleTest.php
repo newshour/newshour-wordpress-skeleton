309fd2e57a6f71bd9bebae753242b81bb1037bfa
Stephan Rohde <srohde@newshour.org>
2021-08-27 12:37:27 -0400

    PhpDoc formatting

--- web/app/themes/core/src/Tests/ExampleTest.php
+++ web/app/themes/core/src/Tests/ExampleTest.php
@@ -1,17 +1,20 @@
 <?php
+
 /**
- * An example test case which tests if the WP_ENV environment variable exists.
- *
- * ./vendor/bin/phpunit --bootstrap vendor/autoload.php web/app/themes/core/src/Tests/ExampleTest
- *
  * @version 1.0.0
  */
+
 namespace App\Cron\Tests;
 
 use Env\Env;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * An example test case which tests if the WP_ENV environment variable exists.
+ *
+ * ./vendor/bin/phpunit --bootstrap vendor/autoload.php web/app/themes/core/src/Tests/ExampleTest
+ */
 class ExampleTest extends TestCase {
 
     public function testEnvExists(): void {
