74e3387d0d77c16e8e13eec6851f60ba9edcfcc5
Stephan Rohde <srohde@newshour.org>
2021-11-03 15:05:42 -0400

    Moving jquery to external lib.

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,21 +3,24 @@ const srcDir = './frontend/src';
 const publicPath = './web/static';
 
 mix
-.disableNotifications()
-.autoload({
-    jquery: ['$', 'window.jQuery']
+.webpackConfig({
+    externals: {
+        jquery: "jQuery"
+    }
 })
+.disableNotifications()
 .options({
     processCssUrls: false
 })
-.js(srcDir + '/js/login.js', '/dist')
-.js(srcDir + '/js/app.js', '/dist')
-.sass(srcDir + '/css/app.scss', '/dist').options({
+.setPublicPath(publicPath)
+.copy('./node_modules/jquery/dist/jquery.min.js', publicPath + '/dist/js')
+.js(srcDir + '/js/login.js', '/dist/js')
+.js(srcDir + '/js/app.js', '/dist/js')
+.sass(srcDir + '/css/app.scss', '/dist/css').options({
     postCss: [
         require('postcss-css-variables')()
     ]
 })
-.setPublicPath(publicPath)
 .extract()
 .version()
 .browserSync({
