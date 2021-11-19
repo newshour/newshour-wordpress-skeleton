8fee26716d8b88540ad1a80425e3c1f2fef32ae3
Stephan Rohde <srohde@newshour.org>
2021-11-19 11:08:22 -0500

    Adding new component lib examples.

--- web/app/themes/core/src/Http/Controllers/Pages/PageController.php
+++ web/app/themes/core/src/Http/Controllers/Pages/PageController.php
@@ -8,21 +8,33 @@ namespace App\Themes\CoreTheme\Http\Controllers\Pages;
 
 use NewsHour\WPCoreThemeComponents\Contexts\Context;
 use NewsHour\WPCoreThemeComponents\Controllers\Controller;
+use NewsHour\WPCoreThemeComponents\Components\Meta\MetaFactory;
 
 /**
  * A controller for pages.
  */
 class PageController extends Controller {
 
-    // The Context object.
     private Context $context;
 
     /**
      * @param Context $context
+     * @param MetaFactory $metaFactory
      */
-    public function __construct(Context $context) {
+    public function __construct(Context $context, MetaFactory $metaFactory) {
 
         $this->context = $context;
+
+        $post = $this->context['post'];
+
+        if ($post != null) {
+            add_action('wp_head', function() use ($post, $metaFactory) {
+                echo implode(PHP_EOL, [
+                    (string) $metaFactory->getPageMeta($post),
+                    (string) $metaFactory->schemas()->getWebPageSchema($post)->asHtml()
+                ]);
+            });
+        }
 
     }
 
