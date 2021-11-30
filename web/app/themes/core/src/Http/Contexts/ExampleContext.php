5aa148c6dc216818ed97882a54468bd8e85396c1
Stephan Rohde <srohde@newshour.org>
2021-11-30 09:59:53 -0500

    Adding example context.

---
+++ web/app/themes/core/src/Http/Contexts/ExampleContext.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @version 1.0.0
+ */
+
+namespace App\Themes\CoreTheme\Http\Contexts;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use NewsHour\WPCoreThemeComponents\Contexts\BaseContext;
+
+/**
+ * An example custom context which extends BaseContext.
+ */
+class ExampleContext extends BaseContext {
+
+    /**
+     * @param Request $request
+     * @param array $initial
+     */
+    public function __construct(Request $request, array $initial) {
+
+        // These key/value pairs will be available for this context and can be accessed in
+        // the controller or in the view template.
+
+        $initial['foo'] = 'bar';
+        $initial['environment'] = WP_ENV;
+        $initial['page_title'] = wp_title(TITLE_SEPARATOR, false, 'right');
+
+        if (is_singular() && is_iterable($posts = $initial['posts'])) {
+            $initial['post'] = $posts[0];
+        }
+
+        parent::__construct($request, $initial);
+
+    }
+
+}
